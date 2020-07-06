86625c9e15cb82af91ebb703456fafd34a6a4d77
AmauriMarcos <amaurisantos.m@gmail.com>
2020-07-06 11:55:13 +0200

    changes on the form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -11,7 +11,7 @@
             </div>
             <div v-if='!isSubmit'  class="box1" v-scrollanimation>
                 <h3>Send me a message</h3>
-                <form class="form" @submit.prevent="sendData">
+                <form class="form" action="POST" data-netlify="true" @submit.prevent="sendData">
                     <ul>
                         <li>
                             <label for="name">Name</label>
